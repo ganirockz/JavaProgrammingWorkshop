ff9952448452f766c449f9025fabca8d3e2aebbb
ganirockz <ganeshrapeti8@gmail.com>
2020-10-07 12:11:14 +0530

    [Ganesh]Added:check if the selected index is free

--- src/org/example/TicTacToeGame.java
+++ src/org/example/TicTacToeGame.java
@@ -6,35 +6,44 @@ public class TicTacToeGame {
 	static char[] BOARD;
 	static Scanner sc = new Scanner(System.in);
 	static int INDEX;
+	static char PLAYER = ' ';
+	static char COMPUTER = ' ';
+
 	public static void main(String[] args) {
-		char player = ' ';
-		char computer = ' ';
+
 		System.out.println("Welcome to TicTacToe game");
 		createBoard();
-		char input = playerInput();
-		player = input;
-		if (input == 'X') {
-			computer = 'O';
+		PLAYER = playerInput();
+
+		if (PLAYER == 'X') {
+			COMPUTER = 'O';
 		} else {
-			computer = 'X';
+			COMPUTER = 'X';
 		}
-		System.out.println("player mark: " + player);
-		System.out.println("computer mark: " + computer);
+		System.out.println("player mark: " + PLAYER);
+		System.out.println("computer mark: " + COMPUTER);
 		showBoard();
-		System.out.println("Enter the index from 1 to 9 where you want to place your move");
-		INDEX = sc.nextInt();
-		selectIndex(INDEX);
+		selectIndex();
 		showBoard();
 	}
-	public static void selectIndex(int index) {
-		if(BOARD[index] == ' ') {
-			System.out.println("Its valid move");
-			BOARD[index] = 'X';
-		}
-		else {
-			System.out.println("Already occupied! please select another index");
+
+	public static void selectIndex() {
+		while (true) {
+			System.out.println("Enter the index from 1 to 9 where you want to place your move");
+			INDEX = sc.nextInt();
+			if (isFreeSpace(BOARD, INDEX)) {
+				System.out.println("Its valid move");
+				BOARD[INDEX] = PLAYER;
+				break;
+			} else {
+				System.out.println("Already occupied! please select another index");
+			}
 		}
 	}
+	public static boolean isFreeSpace(char[] board,int index) {
+		return board[index] == ' ';
+	}
+
 	public static void createBoard() {
 		BOARD = new char[10];
 		for (int i = 1; i < 10; i++) {
@@ -47,11 +56,12 @@ public class TicTacToeGame {
 		char input = sc.next().charAt(0);
 		return input;
 	}
+
 	public static void showBoard() {
-		System.out.println(BOARD[1]+" | "+BOARD[2]+" | "+BOARD[3]);
+		System.out.println(BOARD[1] + " | " + BOARD[2] + " | " + BOARD[3]);
 		System.out.println("----------");
-		System.out.println(BOARD[4]+" | "+BOARD[5]+" | "+BOARD[6]);
+		System.out.println(BOARD[4] + " | " + BOARD[5] + " | " + BOARD[6]);
 		System.out.println("----------");
-		System.out.println(BOARD[7]+" | "+BOARD[8]+" | "+BOARD[9]);
+		System.out.println(BOARD[7] + " | " + BOARD[8] + " | " + BOARD[9]);
 	}
 }
