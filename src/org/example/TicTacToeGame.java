6150efbf53208200dc4a61bbb3979d87a964a4e5
ganirockz <ganeshrapeti8@gmail.com>
2020-10-07 09:56:05 +0530

    [Ganesh]Modified: Allowing player to choose input as 'X' or 'O'

--- src/org/example/TicTacToeGame.java
+++ src/org/example/TicTacToeGame.java
@@ -1,40 +1,35 @@
 package org.example;
+
 import java.util.*;
+
 public class TicTacToeGame {
 	static char[] BOARD;
 	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		char player,computer;
+		char player = ' ';
+		char computer = ' ';
 		System.out.println("Welcome to TicTacToe game");
 		createBoard();
-		System.out.println("Enter who has to place the input:1)player\n2)computer");
-		int option = Integer.parseInt(sc.nextLine());
 		char input = playerInput();
-		if(option == 1) {
-			player = input;
-			if(input == 'X') {
-				computer = 'O';
-			}
-			else {
-				computer = 'X';
-			}
+		player = input;
+		if (input == 'X') {
+			computer = 'O';
+		} else {
+			computer = 'X';
 		}
-		else {
-			computer = input;
-			if(input == 'X') {
-				player = 'O';
-			}
-			else {
-				computer = 'X';
-			}
-		}
+
+		System.out.println("player mark: " + player);
+		System.out.println("computer mark: " + computer);
 	}
+
 	public static void createBoard() {
-		BOARD= new char[10];
-		for(int i=1;i<10;i++) {
+		BOARD = new char[10];
+		for (int i = 1; i < 10; i++) {
 			BOARD[i] = ' ';
 		}
 	}
+
 	public static char playerInput() {
 		System.out.println("Choose a letter 'X' or 'O' as input:");
 		char input = sc.next().charAt(0);
